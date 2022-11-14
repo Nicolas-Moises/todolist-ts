5fb744d2341348ecbac51c2851e1c41ff686d0fe
Nicolas Moises <nicolas.nicc21@gmail.com>
2022-11-14 20:57:27 -0300

    first upgrade, add a localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Header } from "./components/Header";
 import { NewTask } from "./components/NewTask";
@@ -6,6 +6,8 @@ import { Tasks } from "./components/Tasks";
 
 import styles from "./App.module.css"
 import "./global.css";
+
+const LOCAL_STORAGE_KEY = "toSaveTasks"
 
 export interface TasksProps {
   id: string;
@@ -16,6 +18,23 @@ export interface TasksProps {
 function App() {
   const [task, setTask] = useState<TasksProps[]>([])
 
+  useEffect(() => {
+    showTasksOnLocalStorage();
+  }, [])
+
+  function saveTasksOnLocalStorage(newTasks: TasksProps[]) {
+    setTask(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks))
+  }
+
+  function showTasksOnLocalStorage() {
+    const saveItems = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(saveItems) {
+      setTask(JSON.parse(saveItems))
+    }
+    
+  }
+
   function toggleTaskDone(taskId : string) {
     const newTask = task.map((task) => {
       if(task.id == taskId){
@@ -25,13 +44,13 @@ function App() {
       }
       return task;
     })
-    setTask(newTask)
+    saveTasksOnLocalStorage(newTask)
   } 
 
   
 
   function addNewTask(taskContent: string) {
-    setTask([...task, 
+    saveTasksOnLocalStorage([...task, 
       { 
         id: crypto.randomUUID(), 
         content: taskContent,
@@ -44,7 +63,7 @@ function App() {
     const newTasks = task.filter((task) => {
       return task.id != taskId;
     })
-    setTask(newTasks)
+    saveTasksOnLocalStorage(newTasks)
   }
   
   return (
